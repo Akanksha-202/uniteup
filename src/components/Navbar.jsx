9e60e34e8cb994944bff11094df2fdb6ce2ebd7c
Akanksha <akanksha012btit21@igdtuw.ac.in>
2023-06-11 14:53:40 +0530

    Initialization

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar className="flex justify-between">
+        <Typography variant="h6" component="div">
+          My App
+        </Typography>
+        <div className="flex items-center">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="text-white">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;

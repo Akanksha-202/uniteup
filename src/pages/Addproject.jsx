fcf710a5ce8e82675c3979b768a65b163de82887
Akanksha <akanksha012btit21@igdtuw.ac.in>
2023-06-14 22:29:41 +0530

    Project adding backend

--- src/pages/Addproject.jsx
+++ src/pages/Addproject.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import styles from '../CSS/addproject.module.css';
+import { db } from '../config';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Addproject = () => {
+  const [projectName, setProjectName] = useState('');
+  const [projectDomain, setProjectDomain] = useState('');
+  const [membersRequired, setMembersRequired] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+
+  const handleFormSubmit = async () => {
+    try {
+      // Create a reference to the Firebase Firestore collection
+      const projectsCollection = collection(db, 'projects');
+
+      // Create a new project document
+      const newProject = {
+        projectName,
+        projectDomain,
+        membersRequired,
+        projectDescription,
+      };
+
+      // Store the new project document in the Firestore collection
+      await addDoc(projectsCollection, newProject);
+
+      // Reset the form fields after successful submission
+      setProjectName('');
+      setProjectDomain('');
+      setMembersRequired('');
+      setProjectDescription('');
+
+      // Redirect to the desired page
+      window.location.href = '/project';
+    } catch (error) {
+      // Handle any errors that occur during the submission
+      console.error('Error storing project:', error);
+    }
+  };
 
   return (
     <div className={styles.desktop6}>
@@ -10,15 +46,30 @@ const Addproject = () => {
       <b className={styles.addProjects}>ADD PROJECT</b>
 
       <div className={styles.projectName}>
-        <TextField label="Project Name" variant="outlined" />
+        <TextField
+          label="Project Name"
+          variant="outlined"
+          value={projectName}
+          onChange={(event) => setProjectName(event.target.value)}
+        />
       </div>
 
       <div className={styles.projectDomain}>
-        <TextField label="Project Domain" variant="outlined" />
+        <TextField
+          label="Project Domain"
+          variant="outlined"
+          value={projectDomain}
+          onChange={(event) => setProjectDomain(event.target.value)}
+        />
       </div>
 
       <div className={styles.membersRequired}>
-        <TextField label="Members Required" variant="outlined" />
+        <TextField
+          label="Members Required"
+          variant="outlined"
+          value={membersRequired}
+          onChange={(event) => setMembersRequired(event.target.value)}
+        />
       </div>
 
       <div className={styles.projectDescription}>
@@ -27,13 +78,18 @@ const Addproject = () => {
           variant="outlined"
           multiline
           rows={4}
+          value={projectDescription}
+          onChange={(event) => setProjectDescription(event.target.value)}
         />
       </div>
 
-      
-
       <div>
-        <Button variant="contained" color="secondary" className={styles.add} onClick={() => window.location.href = '/project'}>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={styles.add}
+          onClick={handleFormSubmit}
+        >
           ADD
         </Button>
       </div>
@@ -43,7 +99,6 @@ const Addproject = () => {
           CANCEL
         </Button>
       </div>
-
     </div>
   );
 };
